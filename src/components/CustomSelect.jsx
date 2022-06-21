7bc3df094ecd45a1fd22481ce59c4e601f8bb05d
deysan <ideysan@gmail.com>
2022-06-22 00:24:25 +0300

    Add custom select

---
+++ src/components/CustomSelect.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import Select from 'react-select';
+
+export const CustomSelect = styled(Select).attrs({
+  styles: {
+    control: (provided) => ({
+      ...provided,
+      height: '50px',
+      padding: '0.25rem',
+      color: 'var(--colors-text)',
+      backgroundColor: 'var(--colors-ui-base)',
+      borderRadius: 'var(--radii)',
+      border: 'none',
+      boxShadow: 'var(--shadow)',
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      color: 'var(--colors-text)',
+      backgroundColor: state.isSelected
+        ? 'var(--colors-bg)'
+        : 'var(--colors-ui-base)',
+      cursor: 'pointer',
+    }),
+  },
+})`
+  width: 200px;
+  font-family: var(--family);
+  border-radius: var(--radii);
+  border: none;
+
+  & > * {
+    box-shadow: var(--shadow);
+  }
+
+  & input {
+    padding-left: 0.25rem;
+  }
+
+  & * {
+    color: var(--colors-text) !important;
+  }
+
+  & > div[id] {
+    background-color: var(--colors-ui-base);
+  }
+`;
