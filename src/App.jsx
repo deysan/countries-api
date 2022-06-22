02fa36a0d875ff455c3e747301d5758b606bb69d
deysan <ideysan@gmail.com>
2022-06-22 12:27:47 +0300

    Add country list display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import { Card } from './components/Card';
 import { Controls } from './components/Controls';
 import { Header } from './components/Header';
+import { List } from './components/List';
 import { Main } from './components/Main';
 
+import { ALL_COUNTRIES } from './config';
+
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
+  }, []);
+
   return (
     <>
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((country) => {
+            const countryInfo = {
+              img: country.flags.png,
+              name: country.name.common,
+              info: [
+                {
+                  title: 'Population',
+                  description: country.population.toLocaleString(),
+                },
+                {
+                  title: 'Region',
+                  description: country.region,
+                },
+                {
+                  title: 'Capital',
+                  description: country.capital[0],
+                },
+              ],
+            };
+
+            return <Card key={countryInfo.name} {...countryInfo} />;
+          })}
+        </List>
       </Main>
     </>
   );
