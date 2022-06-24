f14013fd2ad10c28a77c913b5ce11d7aa8041842
deysan <ideysan@gmail.com>
2022-06-24 22:50:03 +0300

    Add redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
 import axios from 'axios';
 import { createStore, compose, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 
 import { rootReducer } from './rootReducer';
 import * as api from '../config';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['theme', 'countries'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeEnhancers(
     applyMiddleware(
       thunk.withExtraArgument({
@@ -19,4 +29,6 @@ const store = createStore(
   )
 );
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor };
